8016f6d1008c235f956958d6c23adcbcdc2d6be9
todoggler <kentokuraCS@gmail.com>
2021-10-10 20:10:54 +0900

    constantsを無視する

--- constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    # 各パラメータの設定は自身のものをご利用ください。
-    'consumer_key' => "xxxx",
-    'consumer_secret' => "xxxx",
-    'access_token' => "xxxx",
-    'access_token_secret' => "xxxx", 
-];
